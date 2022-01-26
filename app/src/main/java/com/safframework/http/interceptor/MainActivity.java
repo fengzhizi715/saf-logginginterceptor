04f898f929c0120ed51c3feb35d8749f08054ee6
Tony Shen <fengzhizi715@126.com>
2022-01-26 14:23:44 +0800

    Update MainActivity.java

--- app/src/main/java/com/safframework/http/interceptor/MainActivity.java
+++ app/src/main/java/com/safframework/http/interceptor/MainActivity.java
@@ -4,13 +4,6 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import java.io.IOException;
-
-import okhttp3.Call;
-import okhttp3.Callback;
-import okhttp3.Request;
-import okhttp3.Response;
-
 public class MainActivity extends AppCompatActivity {
 
     @Override
