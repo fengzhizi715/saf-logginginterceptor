17d5da01d1fbd3d5de2577a82e8221cb6b6f3f08
Tony Shen <fengzhizi715@126.com>
2024-10-20 14:44:12 +0800

    update

--- lib/src/main/java/com/safframework/http/interceptor/AndroidLoggingInterceptor.kt
+++ lib/src/main/java/com/safframework/http/interceptor/AndroidLoggingInterceptor.kt
@@ -14,30 +14,35 @@ object AndroidLoggingInterceptor {
 
     @JvmOverloads
     @JvmStatic
-    fun build(isDebug:Boolean = true, hideVerticalLine:Boolean = false, requestTag:String = "Request", responseTag:String = "Response"):LoggingInterceptor {
+    fun build(isDebug:Boolean = true, hideVerticalLine:Boolean = false, requestTag:String = "Request", responseTag:String = "Response", excludeList:MutableList<String> = mutableListOf()):LoggingInterceptor {
 
         init()
 
+        val build = LoggingInterceptor.Builder()
+
+        if (excludeList.isNotEmpty()) {
+            build.excludeList.addAll(excludeList)
+        }
+
         return if (hideVerticalLine) {
-            LoggingInterceptor.Builder()
-                    .loggable(isDebug) // TODO: 发布到生产环境需要改成false
-                    .androidPlatform()
-                    .request()
-                    .requestTag(requestTag)
-                    .response()
-                    .responseTag(responseTag)
-                    .hideVerticalLine()// 隐藏竖线边框
-                    .build()
+            build
+                .loggable(isDebug) // TODO: 发布到生产环境需要改成false
+                .androidPlatform()
+                .request()
+                .requestTag(requestTag)
+                .response()
+                .responseTag(responseTag)
+                .hideVerticalLine()// 隐藏竖线边框
+                .build()
         } else {
-            LoggingInterceptor.Builder()
-                    .loggable(isDebug) // TODO: 发布到生产环境需要改成false
-                    .androidPlatform()
-                    .request()
-                    .requestTag(requestTag)
-                    .response()
-                    .responseTag(responseTag)
-//                    .hideVerticalLine()// 隐藏竖线边框
-                    .build()
+            build
+                .loggable(isDebug) // TODO: 发布到生产环境需要改成false
+                .androidPlatform()
+                .request()
+                .requestTag(requestTag)
+                .response()
+                .responseTag(responseTag)
+                .build()
         }
     }
 }
